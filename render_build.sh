7de065761239babe70268bd1fc7f900285e503c2
Santiago Montoya <santiago.mordz@gmail.com>
2025-05-29 19:40:04 +0000

    Render_build adjustments

--- render_build.sh
+++ render_build.sh
@@ -6,7 +6,7 @@ set -o errexit
 # npm install
 # npm run build
 
-sudo apt-get update -y && sudo apt-get install -y graphviz libgraphviz-dev
+apt-get install -y graphviz libgraphviz-dev
 
 pipenv install
 
